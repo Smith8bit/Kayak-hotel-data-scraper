7ab9b2c0c349b35cfe394acb20594cc05dc75115
Smith8bit <thesmithdot@gmail.com>
2025-11-20 14:17:52 +0700

    Refactor and Finalize

---
+++ main.py
@@ -0,0 +1,41 @@
+import scrapeFunction as sf
+from selenium import webdriver
+import datetime
+import sqlite3
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+
+# options.add_argument("--headless")
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
+driver = webdriver.Chrome(options=options)
+
+prefecture = [  ('Tokyo','Japan-p22200'),
+                ('Osaka','Japan-p22326'),
+                ('Hokkaido','Japan-p23937'),
+                ('Fukuoka','Japan-p22652'),
+                ('Chiba','Japan-p23829'),
+                ('Kyoto','Japan-p22451'),
+                ('Yamanashi','Japan-p22147'),
+                ('Aichi','Japan-p22694'),
+                ('Nagano','Japan-p22398'),
+                ('Gifu','Japan-p22644'),
+                ('Kanagawa','Japan-p22524'),
+                ('Shizuoka','Japan-p22266')
+            ]
+
+delta = datetime.timedelta(days=1)
+
+for p in prefecture:
+        start_date = datetime.date(2025, 12, 1)
+        # end_date = datetime.date(2026, 1, 1)
+        # while (start_date <= end_date):
+        #         next_date = start_date+delta
+        #         sf.get_hotel_data(driver, p, [start_date,next_date])
+        #         start_date += delta
+        sf.get_hotel_data=(driver, p, start_date+delta)
+
+driver.quit()
